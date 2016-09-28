b766b6676244ab5a73f22657a88de6d45eb2215e
chrswt <chriswt91@gmail.com>
2016-09-27 19:48:36 -0700

    Refactor code

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,7 @@ app.friends = {};
 
 var selectedValue, lastRetrieved;
 
-app.send = (message) => { // pass in message object
+app.send = message => { // pass in message object
   $.ajax({
   // This is the url you should use to communicate with the parse API server.
     url: 'https://api.parse.com/1/classes/messages',
@@ -16,24 +16,21 @@ app.send = (message) => { // pass in message object
       roomname: selectedValue
     }),
     contentType: 'application/json',
-    success: function (data) {
-      // Success
+    success: data => {
     },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    error: data => {
       console.error('chatterbox: Failed to send message', data);
     }
   });
 };
 
-app.renderMessage = (message) => {
+app.renderMessage = message => {
   $('#chats').append('<div class="' + message.roomname + ' room">' + 
     '<a class="' + filterXSS(message.username) + '">' + 
     '<i class="fa fa-user-plus addFriend" aria-hidden="true"></i>' +
     '  @' + filterXSS(message.username) + '</a>' + ' ' + 
-    '<span class="timeago">' + jQuery.timeago(message.createdAt) + '</span>' + '<br>' + 
-    '<input type="checkbox" name="checkboxG4" id="checkboxG4" class="css-checkbox"><label for="checkboxG4" class="css-label radGroup1 clr"></label>' + 
-    filterXSS(message.text) + '</div>');
+    '<span class="timeago">' + jQuery.timeago(message.createdAt) + '</span>' + '<br><span class="message">' +  
+    filterXSS(message.text) + '</span>' + '  ' + '<input type="checkbox" name="checkboxG4" id="checkboxG4" class="css-checkbox"><label for="checkboxG4" class="css-label radGroup1 clr"></label></div>');
 };
 
 app.fetch = () => {
@@ -41,32 +38,35 @@ app.fetch = () => {
   // This is the url you should use to communicate with the parse API server.
     url: 'https://api.parse.com/1/classes/messages',
     type: 'GET',
-    data: {order: '-createdAt', limit: 100},
+    data: {order: '-createdAt', limit: 10},
     contentType: 'application/json',
-    success: function (data) {
+    success: data => {
       if (lastRetrieved) { // page has retrieved data before
 
         if (lastRetrieved === data.results[0].objectId) {
           // fully updated, dont do anything
         } else {
-          var index;
-          for (var i = 0; i < data.results.length; i++) {
-            if (data.results[i].objectId === lastRetrieved) {
+          let index;
+          data.results.forEach(result => {
+            if (result.objectId === lastRetrieved) {
               index = i; // attain index of last updated element
             }
-          }
+          });
 
           // slice and prepend new stuff
-          var newData = data.results.slice(0, index);
-          for (var i = newData.length - 1; i >= 0; i--) {
+          let newData = data.results.slice(0, index);
+
+          //iterating from the end of the array and appending new chats
+          for (let i = newData.length - 1; i >= 0; i--) {
             var message = newData[i];
-            var rendered = '<div class="' + message.roomname + ' room">' + 
-            '<a class="' + filterXSS(message.username) + '">' + 
-            filterXSS(message.username) + '</a>' + '@' + 
-            filterXSS(message.roomname) + ': ' + 
-            filterXSS(message.text) + '<p class="timeago">' +
-            jQuery.timeago(message.createdAt) + '</div>';
             var location = $('#chats').find('div:last-child');
+            var rendered = '<div class="' + message.roomname + ' room">' + 
+              '<a class="' + filterXSS(message.username) + '">' + 
+              '<i class="fa fa-user-plus addFriend" aria-hidden="true"></i>' +
+              '  @' + filterXSS(message.username) + '</a>' + ' ' + 
+              '<span class="timeago">' + jQuery.timeago(message.createdAt) + '</span>' + '<br><span class="message">' +  
+              filterXSS(message.text) + '</span>' + '  ' + '<input type="checkbox" name="checkboxG4" id="checkboxG4" class="css-checkbox">'
+              + '<label for="checkboxG4" class="css-label radGroup1 clr"></label></div>';
 
             $(rendered).hide().insertAfter(location).fadeIn(1000);
           }
@@ -89,26 +89,32 @@ app.fetch = () => {
         app.renderRoom(app.rooms);
       }
 
-
+      //save the name of the room last entered so that it does not get refreshed
       $('.roomselect')[0].selectize.setValue(selectedValue);
 
+      //toggles class for the like button orange fill
       $('.radGroup1').on('click', function() {
         $(this).toggleClass('chk');
       });
 
+      //upon clickin the icon, friend finds the parent div that has the username as a class
+      //adds the friend name as a key-value pair to the friends object
       $('.addFriend').on('click', function() {
         var friend = $(this).parent().closest('a').attr('class');
         app.addFriend(friend);
       });
+
 
       $('.removeFriend').on('click', function() {
         var friend = $(this).parent().closest('a').attr('class');
         app.removeFriend(friend);
       });
 
+      //controls scrollbar
+      $('#chats').animate({scrollTop: 999999}, 2000);
+
     },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    error: data => {
       console.error('chatterbox: Failed to send message', data);
     }
   });
@@ -116,29 +122,36 @@ app.fetch = () => {
 
 
 
-app.clearMessages = () => {
+app.clearRooms = () => {
   $('.selectpicker').empty();
 };
-app.clearRooms = () => {
+app.clearMessages = () => {
   $('#chats').empty();
 };
 
-app.renderRoom = function(roomArray) {
+app.renderRoom = roomArray => {
   var rooms = [];
-  _.uniq(roomArray).forEach(function(room) {
+  //finding unique values in the rooms array
+  _.uniq(roomArray).forEach(room => {
     rooms.push({text: room, value: room});
   });
 
+  //applying the selectize class onto the dropdown menu.
   var selectize = $('.roomselect')[0].selectize;
+ 
   selectize.clear();
   selectize.clearOptions();
-  rooms.forEach(function(room) {
+
+  rooms.forEach(room => {
     selectize.addOption(room);
   });
 };
 
+
 app.addFriend = friend => {
+  //replaces the add friend icon with github icon
   $('.' + friend).find('i').replaceWith('<i class="fa fa-github-alt removeFriend" aria-hidden="true"></i>');
+  //add friend to the friends object
   if (!app.friends.hasOwnProperty(friend) && friend !== undefined) {
     app.friends[friend] = friend;
   }
@@ -154,6 +167,7 @@ app.removeFriend = friend => {
 };
 
 app.updateFriendsList = () => {
+  //refreshes friends list on every fetch
   $('.friends').empty();
   
   for (var key in app.friends) {
@@ -162,12 +176,14 @@ app.updateFriendsList = () => {
 };
 
 $(document).ready(function() {
+  //updates immediately
   app.fetch();
 
   setInterval(function() {
     app.fetch();
   }, 10000);
 
+  //runs the post ajax command on click or on enter
   $('.postmsg').on('keydown click', function(event) {
     if ((event.type === 'keydown' && event.keyCode === 13) || event.type === 'click') {
       event.preventDefault();
@@ -177,15 +193,19 @@ $(document).ready(function() {
     }
   });
 
+  //default settings for the dropdown menu
   $('.roomselect').selectize({
     create: true,
     sortField: 'text'
   });
 
+  //dropdown menu
   $('.roomselect').change(function() {
     selectedValue = ($(this).val()) || selectedValue; // sticky fix!
-    console.log(selectedValue);
+    $('.room').hide();
+    $('.' + selectedValue).show();
+    // $('#chats').animate({scrollTop: 100000}, 2000);
   });
 
-  $('#chats').animate({scrollTop: 10000}, 2000);
+  $('#chats').animate({scrollTop: 100000}, 2000);
 });
